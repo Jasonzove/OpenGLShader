b28186dbc6002b86f550efd3dc5abc95afcdb4a2
kdvictor <huanliu_uestc@163.com>
2020-08-05 07:35:20 +0800

    关闭release优化

---
+++ OpenGLShader/include/GPUProgram.h
@@ -0,0 +1,17 @@
+/*===================================
+*Copyright(c): huan.liu, 2020
+*All rights reversed
+
+*File name: GPUProgram.h
+*Brief: program·â×°
+
+*Data:2020.08.05
+*Version: 1.0
+=====================================*/
+
+#ifndef __GPUPROGRAM_H__
+#define __GPUPROGRAM_H__
+
+
+
+#endif
