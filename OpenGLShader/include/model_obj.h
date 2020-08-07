887e0197c6142dc2ffa01053271ee18423437e1e
kdvictor <huanliu_uestc@163.com>
2020-08-07 08:10:02 +0800

    ObjMoel类封装

---
+++ OpenGLShader/include/model_obj.h
@@ -0,0 +1,20 @@
+/*===================================
+*Copyright(c): huan.liu, 2020
+*All rights reversed
+
+*File name: model_obj.h
+*Brief: obj模型类封装
+
+*Data:2020.08.07
+*Version: 1.0
+=====================================*/
+
+#ifndef __MODEL_OBJ_H__
+#define __MODEL_OBJ_H__
+
+class ObjMoel
+{
+
+};
+
+#endif
