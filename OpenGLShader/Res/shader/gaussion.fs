9b8481618e5f5ac8568edda33812e9a49686ef10
kdvictor <huanliu_uestc@163.com>
2020-08-20 06:45:19 +0800

    高斯模糊

--- OpenGLShader/Res/shader/gaussion.fs
+++ OpenGLShader/Res/shader/gaussion.fs
@@ -6,17 +6,26 @@ in vec2 T_texcoord;
 
 void main()
 {
-    vec4 minValue = vec4(1.0);
-    int coreSize = 2;
-    int halfCore = coreSize/2;
-    float texe10Offset = 1/100.0;
+    //1 2 1
+    //2 4 2
+    //1 2 1
+    vec4 color = vec4(0.0);
+    int coreSize = 3;
+    float texe10Offset = 1/200.0;
+    float kernal[9] = {1,2,1,2,4,2,1,2,1}; //从左下角开始扫描
+    //kernal[6] = 1;kernal[7] = 2;kernal[8] = 1;
+    //kernal[3] = 2;kernal[4] = 4;kernal[5] = 2;
+    //kernal[0] = 1;kernal[1] = 2;kernal[2] = 1;
+
+    int index = 0;
     for(int y = 0; y < coreSize; ++y)
     {
         for(int x = 0; x < coreSize; ++x)
         {
-            vec4 currentColor = texture2D(U_MainTexture, T_texcoord+vec2((-halfCore+x)*texe10Offset,(-halfCore+y)*texe10Offset));  
-            minValue = min(minValue, currentColor);
+            vec4 currentColor = texture2D(U_MainTexture, T_texcoord+vec2((-1+x)*texe10Offset,(-1+y)*texe10Offset));  
+            color += currentColor*kernal[index++];
 		}
 	}
-    gl_FragColor = vec4(minValue.rgb, minValue.a);
+    color /= 16.0;
+    gl_FragColor = color;
 }
